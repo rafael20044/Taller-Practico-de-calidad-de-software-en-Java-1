47876b1454c54d01a96de3364846d2480eaef5ef
Rafael Barragán Acevedo <70608879+rafael20044@users.noreply.github.com>
2025-08-14 21:33:24 -0500

    first commit

---
+++ src/main/java/com/agenda/calculadoracontest/CalculadoraConTest.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.agenda.calculadoracontest;
+
+/**
+ *
+ * @author Rafael Barragán Acevedo
+ */
+public class CalculadoraConTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
