5a3fb1d1e1b4b26e3284ec872086ced9cd52800b
Rafael Barragán Acevedo <70608879+rafael20044@users.noreply.github.com>
2025-08-14 21:57:32 -0500

    se agrego la libreria de JUnit5

---
+++ src/test/java/test/CalculadoraTest.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package test;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Rafael Barragán Acevedo
+ */
+public class CalculadoraTest {
+    @Test
+    public void test() {
+        System.out.println("Hola mundo");
+    }
+    
+}
